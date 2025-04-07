c61cd79d22671d1edc5cd093a286cbf3ec67cc57
bmaitho <bmaitho@gmail.com>
2025-04-07 11:08:38 +0300

    add the ability to see the title

--- Qshop/src/components/ProductDetails.jsx
+++ Qshop/src/components/ProductDetails.jsx
@@ -142,7 +142,7 @@ const ProductDetails = () => {
   return (
     <>
       <Navbar />
-      <div className="max-w-6xl mx-auto p-4">
+      <div className="max-w-6xl mx-auto p-4 pt-20"> 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
           {/* Product Image */}
           <div className="relative">
